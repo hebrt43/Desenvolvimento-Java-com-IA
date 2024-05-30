fd0d20dff757c963cbce135a87fdd11dbb19519e
Hebert Ferreira <117025526+hebrt43@users.noreply.github.com>
2024-05-30 18:59:18 -0300

    comentarios dos operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,37 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        // = perador de atribuição
+        // + (Adição) - (Subitraçao) * (Multiplicaçao) / (Divisão) % (Modulo) operadores aritimeticos 
+        // + (Adição) tambem é utilizado na concatenação de textos.
+        // (+) Operador unario de valor positivo
+        // (-) Operador unario de valor negativo
+        // (++) Operador unario de incremento de valor
+        // (--) Operador unario de decremento de valor
+        // (!) Operador unario de negação de valor
+        // (?:) Operador ternario
+        // (==) Operador Relacional, verifica´verifica se uma variavel é igualdade 
+        // (!=) Operador Relacional, verifica se uma variavel e diferença
+        // (>) Operador Relacional, verifica se uma variavel é maior
+        // (>=) Operador Relacional, verifica se uma variavel é maior ou igual
+        // (<) Operador Relacional, verifica se uma variavel é menor
+        // (<=)  Operador Relacional, verifica se uma variavel é menor ou igual
+        // (&&) Operador Logico "E"
+        // (||) Operador Logico "OU"
+
+
+        double soma = 10.5 + 15.7;
+        int subtraçao = 113 - 25;
+        int Multiplicaçao = 20 * 7;
+        int divisao = 15/3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        System.out.println(soma);
+        System.out.println(subtraçao);
+        System.out.println(Multiplicaçao);
+        System.out.println(divisao);
+        System.out.println(modulo);
+        System.out.println(resultado);
+    }
+}
