9ad7bcef754ee15354691fc9e8070aa456368c6f
Hebert Ferreira <117025526+hebrt43@users.noreply.github.com>
2024-05-30 12:23:04 -0300

    constantes e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,18 @@
 public class TiposVariaveis {
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        double salarioMinimo = 1400;
 
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
         
     
 }
