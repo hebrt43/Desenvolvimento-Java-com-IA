5099cdd3291bbd4f649a3048191a101723e39513
Hebert Ferreira <117025526+hebrt43@users.noreply.github.com>
2024-06-01 09:49:41 -0300

    elaboração do exercicio de controle de tv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolune();
+        smartTv.diminuirVolune();
+        smartTv.diminuirVolune();
+        smartTv.aumentarVolune();
+        smartTv.mudarCanal(21);
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> tv ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> tv ligada ? " + smartTv.ligada);
+
+    }
+}
